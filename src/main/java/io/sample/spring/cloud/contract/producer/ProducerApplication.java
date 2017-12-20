1a956698f467a4b48d7012841931036b0e6689bb
John Smith <ecco_fatto@mail.ru>
2017-12-20 18:35:14 +0200

    Add cloud stream dependencies

--- src/main/java/io/sample/spring/cloud/contract/producer/ProducerApplication.java
+++ src/main/java/io/sample/spring/cloud/contract/producer/ProducerApplication.java
@@ -2,7 +2,10 @@ package io.sample.spring.cloud.contract.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
 
+@EnableBinding(Sink.class)
 @SpringBootApplication
 public class ProducerApplication {
 
